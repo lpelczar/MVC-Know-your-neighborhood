9b7047abfe6e33c70903d63d50c336e75284d512
lpelczar <lukepelczar@gmail.com>
2017-11-14 18:54:21 +0100

    Implement displaying menu

---
+++ view.py
@@ -0,0 +1,11 @@
+class View:
+
+    @staticmethod
+    def display_menu():
+        print('What would you like to do:' +
+              '\t(1) List statistics' +
+              '\t(2) Display 3 cities with longest names' +
+              '\t(3) Display county\'s name with the largest number of communities' +
+              '\t(4) Display locations, that belong to more than one category' +
+              '\t(5) Advanced search' +
+              '\t(0) Exit program')
