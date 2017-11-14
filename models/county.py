d6f0379d00a2253c8528fa7ceb70a5cb3302bea4
lpelczar <lukepelczar@gmail.com>
2017-11-14 19:16:36 +0100

    Add county class

---
+++ models/county.py
@@ -0,0 +1,9 @@
+class County:
+
+    def __init__(self, name, number):
+        self.county_regions = []
+        self.name = name
+        self.number = number
+
+    def add_county_region(self, county_region):
+        self.county_region.append(county_region)
