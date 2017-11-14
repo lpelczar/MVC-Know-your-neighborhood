0917eaa8c07fd2b095ad86b7789c720a7bfdc140
lpelczar <lukepelczar@gmail.com>
2017-11-14 17:17:46 +0100

    Implement reading all regions from file

--- container.py
+++ container.py
@@ -1,3 +1,14 @@
-class RegionsContainer:
+from region import Region
+import csv
 
-    regions = []
+
+class RegionContainer:
+
+    def __init__(self):
+        self.regions = []
+
+    def read_from_file(self):
+        with open('malopolska.csv') as f:
+            reader = csv.DictReader(f, delimiter='\t')
+            for row in reader:
+                self.regions.append(Region(row['woj'], row['pow'], row['gmi'], row['nazwa'], row['typ']))
---
+++ region.py
@@ -0,0 +1,8 @@
+class Region:
+
+    def __init__(self, voivodeship_num, county_num, community_num, name, region_type):
+        self.voivodeship_num = voivodeship_num
+        self.county_num = county_num
+        self.community_num = community_num
+        self.name = name
+        self.region_type = region_type
